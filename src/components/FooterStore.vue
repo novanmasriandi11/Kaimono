1e2f1ce32010552f3cb4ca04bbb0838ff6e9fd1d
novanmasriandi11 <novanmasriandi@gmail.com>
2023-02-05 03:42:06 +0700

    Upload Project VueJS

---
+++ src/components/FooterStore.vue
@@ -0,0 +1,70 @@
+<template>
+  <!-- Footer Section Begin -->
+  <footer class="footer-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5">
+          <div class="footer-left text-left">
+            <div class="footer-logo">
+              <a href="#"><img src="img/logo_website_white.png" alt="" /></a>
+            </div>
+            <ul>
+              <li>
+                Address: 5 Chome-5-番18号 Nishimachi, Higashikawa, Kamikawa
+                District, Hokkaido 071-1425
+              </li>
+              <li>Phone: +817 70111999</li>
+              <li>Email: kaimono@kamikawa.net</li>
+            </ul>
+            <div class="footer-social">
+              <a href="#"><i class="fa fa-facebook"></i></a>
+              <a href="#"><i class="fa fa-instagram"></i></a>
+              <a href="#"><i class="fa fa-twitter"></i></a>
+              <a href="#"><i class="fa fa-pinterest"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 offset-lg-1">
+          <div class="footer-widget text-left">
+            <h5>Information</h5>
+            <ul>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Checkout</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">Service</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="footer-widget text-left">
+            <h5>My Account</h5>
+            <ul>
+              <li><a href="#">My Account</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">Shopping Cart</a></li>
+              <li><a href="#">Shop</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="copyright-reserved">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="copyright-text">
+              Copyright &copy; 2020 - 2023 All rights reserved | 買い物
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- Footer Section End -->
+</template>
+
+<script>
+export default {
+  name: "FooterStore",
+};
+</script>
